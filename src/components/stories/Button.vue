cbb9cc0668c32f54a94b273d38db480e122a9eab
ytk6565 <ytk6565@gmail.com>
2021-06-28 09:25:30 +0900

    feat: sample stories

---
+++ src/components/stories/Button.vue
@@ -0,0 +1,60 @@
+<template>
+  <button type="button" :class="classes" @click="onClick" :style="style">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+import "./button.css";
+
+export default {
+  name: "my-button",
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    primary: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: "medium",
+      // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+      validator: function (value) {
+        return ["small", "medium", "large"].indexOf(value) !== -1;
+      },
+    },
+    backgroundColor: {
+      type: String,
+    },
+  },
+
+  computed: {
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    classes() {
+      return {
+        "storybook-button": true,
+        "storybook-button--primary": this.primary,
+        "storybook-button--secondary": !this.primary,
+        [`storybook-button--${this.size}`]: true,
+      };
+    },
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    style() {
+      return {
+        backgroundColor: this.backgroundColor,
+      };
+    },
+  },
+
+  methods: {
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    onClick() {
+      this.$emit("onClick");
+    },
+  },
+};
+</script>
